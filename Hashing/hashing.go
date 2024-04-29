797fc525cd04b14fb4259d27804d5d75cacf5d30
Frykits <88166479+Frykits@users.noreply.github.com>
2024-04-29 19:00:56 -0300

    hashing a file

--- Hashing/hashing.go
+++ Hashing/hashing.go
@@ -4,13 +4,25 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
-	fmt.Println(getSHA256Hash("bubu"))
+	hash, err := getSHA256Hash("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(hash)
 }
 
-func getSHA256Hash(text string) string {
-	hash := sha256.Sum256([]byte(text))
-	return hex.EncodeToString(hash[:])
+func getSHA256Hash(filepath string) (string, error) {
+	data, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return "", err
+	}
+
+	hash := sha256.Sum256(data)
+	return hex.EncodeToString(hash[:]), nil
+
 }
